e4745fa14ba014f633441b5fce705e6386c67ce2
jtn <>
2019-12-28 20:13:22 +0200

    document public apis

--- lib/flutter_manifest.dart
+++ lib/flutter_manifest.dart
@@ -5,7 +5,13 @@ import 'package:flutter/services.dart' show rootBundle;
 
 import 'models/web_manifest.dart';
 
+/// Provides web manifest information. manifest.json file must be in assets.
+/// ```dart
+/// WebManifest manifest = await Manifest.read()
+/// print("Version is: ${manifest.version}");
+/// ```
 class Manifest {
+  /// Retrieves manifest information from the manifest.json.
   static Future<Map<String, dynamic>> readJson() async {
     try {
       String manifestJson = await rootBundle.loadString('manifest.json');
@@ -16,6 +22,7 @@ class Manifest {
     }
   }
 
+  /// Retrieves manifest information from the manifest.json.
   static Future<WebManifest> read() async {
     final json = await Manifest.readJson();
     if (json != null) {
